570cffa0f8ad97c496276b1fcb147171730d7986
Wojciech Balda <wojtek.balda@gmail.com>
2024-05-22 18:29:06 +0200

    Add utilities functions
    sortByDate - Function sorts posts from newest to oldest
    createPagination - Function creates array that can be used to build a pagination component
    generateHref - Function generates href value for pagination component

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
+import type { CollectionEntry } from "astro:content";
 import type { ClassValue } from "clsx";
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
@@ -5,4 +6,25 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[])
 {
     return twMerge(clsx(inputs))
+}
+
+export function sortByDate(a: CollectionEntry<"posts">, b: CollectionEntry<"posts">)
+{
+    return b.data.date.getTime() - a.data.date.getTime()
+}
+
+export function createPagination(numberOfPages: number, currentPage: number)
+{
+    if (numberOfPages < 7)
+        return Array.from({length: numberOfPages}, (_, i) => i + 1)
+    if (currentPage < 4)
+        return [1, 2 , 3, 4, '..', numberOfPages - 1, numberOfPages]
+    if (currentPage > numberOfPages - 3)
+        return [1,2, '..', numberOfPages - 3, numberOfPages - 2, numberOfPages - 1, numberOfPages]
+    return [1, '...', currentPage - 1, currentPage, currentPage + 1, '..', numberOfPages]
+}
+
+export function generateHref(url: string, page: number)
+{   
+    return page === 1 ? url : `${url}${page}`
 }
